3923bb0b14a65549ddcd1ba25301d49491a6cf39
Ivan Ivanov <csyntax@outlook.com>
2017-11-30 16:54:20 +0200

    configure VS Code and create basic server

--- src/app/index.js
+++ src/app/index.js
@@ -3,3 +3,23 @@ const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const morgan = require("koa-morgan");
 
+module.exports = () => {
+    const app = new Koa();
+    const router = new Router();
+
+    router
+        .get("/", async(ctx, next) => { 
+            ctx.body = Date.now();
+            ctx.status = 200;
+
+            await next();
+        });
+
+    app.use(bodyParser());
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+
+    return new Promise(resolve => {
+        app.listen(3000, resolve);
+    });
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const runServer = async() => {
-        
+    const app = require("./app")();
+
+    console.log(`App running at 3000`);
 };
 
 runServer();
