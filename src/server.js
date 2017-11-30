52d01f66800b22faede8f4604021411ed57e0822
Ivan Ivanov <csyntax@outlook.com>
2017-11-30 17:26:44 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const runServer = async() => {
     const app = require("./app")(process.env.PORT || 6000);
 
-    console.log(`App running at ${process.env.PORT || 3000}`);
+    console.log(`App running at ${process.env.PORT || 6000}`);
 };
 
 runServer();
