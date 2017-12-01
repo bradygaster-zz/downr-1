c15342e490a390f452e5fadba23c8a25b5bd1d20
Ivan Ivanov <csyntax@outlook.com>
2017-12-01 23:01:02 +0200

    readFile asyncfunction

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,7 +4,8 @@ module.exports = router => {
     router
         .get("/", async(ctx, next) => { 
             const posts = await postService();
-
+            
+            ctx.type = 'text/plain; charset=utf-8';
             ctx.body = posts;
             ctx.status = 200;
 
--- src/services/posts.js
+++ src/services/posts.js
@@ -1,16 +1,34 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
 const POSTS_DIR = path.join(__dirname, "..", "..", "posts");
 
+const readFile = function async(filename) {
+    return new Promise(function (resolve, reject) {
+        try {
+            fs.readFile(filename, "utf-8", function(err, buffer){
+                if (err) reject(err); else resolve(buffer);
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
 module.exports = async() => {
+    
     return new Promise(resolve => {
         fs.readdir(POSTS_DIR, async(err, data) => {
             if (err) {
                 throw err;
             }
-            
-            resolve(data);     
-       });
+            let files = [];
+
+            for (let folder of data) {
+                files.push(await readFile(path.join(POSTS_DIR, folder, "index.md")));
+            }
+
+            resolve(files);            
+        });
    });
 };
