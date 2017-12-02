53f872369f3c2cbb1fbc2b0acd784e7858f32bc9
Ivan Ivanov <csyntax@outlook.com>
2017-12-02 08:52:47 +0200

    update posts service

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,10 +4,14 @@ const path = require("path");
 const POSTS_DIR = path.join(__dirname, "..", "..", "posts");
 
 const readFile = function async(filename) {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
         try {
-            fs.readFile(filename, "utf-8", function(err, buffer){
-                if (err) reject(err); else resolve(buffer);
+            fs.readFile(filename, "utf-8", (err, file) => {
+                if (err) {
+                    reject(err); 
+                }
+                
+                resolve(file);
             });
         } catch (err) {
             reject(err);
@@ -16,13 +20,13 @@ const readFile = function async(filename) {
 };
 
 module.exports = async() => {
-    
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
+        let files = [];
+
         fs.readdir(POSTS_DIR, async(err, data) => {
             if (err) {
-                throw err;
+                reject(err);
             }
-            let files = [];
 
             for (let folder of data) {
                 files.push(await readFile(path.join(POSTS_DIR, folder, "index.md")));
