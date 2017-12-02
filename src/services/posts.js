79c787b91e27b2b22175e8816a6214aa098d4b17
Ivan Ivanov <csyntax@outlook.com>
2017-12-02 09:34:30 +0200

    use arrow functions

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,12 +4,12 @@ const matter = require("gray-matter");
 
 const POSTS_DIR = path.join(__dirname, "..", "..", "posts");
 
-const readFile = function async(filename) {
+const readFile = async(filename) => {
     return new Promise((resolve, reject) => {
         try {
             fs.readFile(filename, "utf-8", (err, file) => {
                 if (err) {
-                    reject(err); 
+                    reject(err);
                 }
                 
                 resolve(matter(file));
