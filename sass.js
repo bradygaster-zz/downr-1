e708d46f0580219828d26dd81c1b5b91d2ab6d2f
Ivan Ivanov <csyntax@outlook.com>
2017-12-10 12:34:19 +0200

    create sass compiler

--- sass.js
+++ sass.js
@@ -1,3 +1,42 @@
+const fs = require("fs");
+const path = require("path");
 const sass = require("node-sass");
 
 console.log("Compiling sass");
+
+const PAGES_DIR_STYLES = path.join(__dirname, "pages", "sass");
+const CSS_STYLES = path.join(__dirname, "public", "css", "styles.css");
+
+let content = "";
+
+fs.readdir(PAGES_DIR_STYLES, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(PAGES_DIR_STYLES, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+        console.log("%s (%s)", file, path.extname(file));
+
+        sass.render({
+        	file: file
+        }, function(err, result) {
+        	if (err) {
+        		throw err;
+    		}
+
+    		content += result.css.toString();
+
+    		fs.writeFile(CSS_STYLES, content, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
+
+			    console.log("Add pages styles");
+			}); 
+        });
+    });
+});
