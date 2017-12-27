abe8254e108d4a604138c288be607e911ac29ebb
Ivan Ivanov <csyntax@outlook.com>
2017-12-27 22:11:11 +0200

    update server.js

--- server.js
+++ server.js
@@ -8,10 +8,3 @@ http.createServer(app().callback()).listen(PORT);
 
 console.log(`You are using ${os.platform()}`);
 console.log(`Application running on ${PORT}`);
-
-
-
-/*
-require("./app")(PORT).then(resolve => {
-    console.log(`App running on port: ${PORT}`);
-});*/
